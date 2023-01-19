34494a272d329b95ee6781a438c91143e2d1b61c
KristynaPtacnikova <122735600+KristynaPtacnikova@users.noreply.github.com>
2023-01-19 20:47:53 +0100

    Zmeny

--- prvniUkol.py
+++ prvniUkol.py
@@ -2,3 +2,5 @@
 #print(jmeno_a_prijmeni[0].title()) takhle kod funguje,ale pouze na jedno slovo 
 #jmeno_a_prijmeni = (input("Jak se jmenuješ?"))
 #print(jmeno_a_prijmeni[x[0].title())
+
+#########v#
