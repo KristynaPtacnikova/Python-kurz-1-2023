db64dc9c9d1aab0be8a3d4a6f8cb00fd22d5ed58
KristynaPtacnikova <122735600+KristynaPtacnikova@users.noreply.github.com>
2023-04-02 20:41:09 +0200

    Add files via upload

---
+++ ukol_04-konecna_verze.py
@@ -0,0 +1,31 @@
+import math
+
+def telefonni_cislo(vloz_cislo:int):#Overi spravnou delku cisla
+    max_delka = 13
+    text = input(str("Zadejte text:"))
+    delka = len(vloz_cislo)
+    if delka == max_delka and (vloz_cislo[slice(+420)]):
+            return True
+    elif delka == 9:
+            return True
+    elif telefonni_cislo(vloz_cislo) == True:
+            return(input("Zadejte text:"))
+    else:
+            return False
+
+#print(telefonni_cislo(vloz_cislo))
+
+def cena_sms(text:str):# Vykalkuluje nam cenu za pocet(180) znaku
+    text_delka = len(text)
+    zaokrouhleni = (math.ceil(text_delka/180))
+    cena = int(zaokrouhleni)*3
+    return(cena)
+vloz_cislo = input(f"Zadejte tel.číslo:")
+# Overime, jestli je platne pomoci nasi funkce telefonni_cislo:
+platne_cislo = telefonni_cislo(vloz_cislo)
+
+# Pokud ano, zeptame se na text zpravy a spocitame cenu 
+if platne_cislo:
+    text_sms = input("Zadejte text:")
+    print(f"Vaše sms stojí {cena_sms(text_sms)} Kč.")
+
